a8ff554746dad4be71904ab25e86668cea3d15e6
Tae Kim <tk382@uchicago.edu>
2017-12-08 16:36:44 -0600

    comment + minor changes

--- rmsctest.R
+++ rmsctest.R
@@ -7,6 +7,8 @@ library(dplyr)
 library(reshape)
 library(caret)
 library(fossil)
+
+R.utils::sourceDirectory("R/")
 data("BuettnerFlorian")
 X = BuettnerFlorian$in_X
 truth = BuettnerFlorian$true_labs
@@ -21,36 +23,26 @@ X3 = X[reshuffle[5993:8989],]
 T1 = constructKernel(t(X1), 5)
 T2 = constructKernel(t(X2), 5)
 T3 = constructKernel(t(X3), 5)
+
+#just for visualization - diagonals popping out too much
 diag(T3) = colMeans(T3)
 TT = melt(T3)
+
+#see how different the kernel matrix looks for each split.
+#is the error sparse? should i put 2norm penalty instead? => seems sparse to me
 ggplot(TT, aes(x=X1, y=X2, fill=value)) + geom_tile()+scale_fill_gradient(high='indianred', low='antiquewhite1')
 T = array(0,dim=c(182,182,3))
 T[,,1] = T1; T[,,2] = T2; T[,,3] = T3
 
-lambda = 0.005
+lambda = 0.005  #how do I optimize this?
 verbose=TRUE
 eps = 1e-9
 max_iter = 300
 rm(T1, T2, T3)
 
-
+#run RMSC
 P = RMSC(T, 0.05, verbose=TRUE)
 ggplot(melt(P), aes(x=X1, y=X2, fill=value))+geom_tile()+scale_fill_gradient(high='indianred', low='antiquewhite1', lim=c(0.00, 0.03))
 res = baseline_spectral(P, numClust=3)
-plot(truth$V1 ~ res)
 table(truth$V1, res)
-
-
-rand.index(truth$V1, res)
-
-
-cbind(truth$V1, res)
-
-for (i in 1:length(res)){
-  if(res[i]==3){res[i] <- 2}
-  else if(res[i]==1){res[i] <-3}
-  else{res[i] <- 1}
-}
-
-
-table(res, truth$V1)
+rand.index(truth$V1, res) #0.878
