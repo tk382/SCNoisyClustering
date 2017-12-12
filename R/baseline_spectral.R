0e6f752946cdc9e7f030ef67232e587329e852bb
Tae Kim <tk382@uchicago.edu>
2017-12-12 10:42:52 -0600

    minor vectorization

--- R/baseline_spectral.R
+++ R/baseline_spectral.R
@@ -1,13 +1,10 @@
-baseline_spectral = function(Phat, numClust, truth){
+baseline_spectral = function(Phat, numClust){
   tmp = irlba(Phat, numClust)
   U = tmp$u
   norm_mat = matrix(rep(sqrt(rowSums(U^2)),numClust), ncol=numClust)
-  for (i in 1:nrow(norm_mat)){
-    if(norm_mat[i,1]==0){norm_mat[i,] = 1}
-  }
+  errorind = which(norm_mat[,1]==0)
+  norm_mat[errorind, ] = 1
   U = U/norm_mat
-  for (i in 1:10){
-    C = kmeans(U, numClust)
-  }
+  C = kmeans(U, numClust)
   return(C$cluster)
 }
