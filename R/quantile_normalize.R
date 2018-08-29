e5ef2cf1d7476e5d37dc62898e29937046763631
tk382 <tk382@uchicago.edu>
2018-08-29 11:25:00 -0500

    add normalization function

---
+++ R/quantile_normalize.R
@@ -0,0 +1,11 @@
+#' quantile normalize the gene expression level matrix
+#'
+#' @param X matrix to quantile-normalize
+#' @export
+quantile_normalize = function(X){
+  X.rank = apply(X, 2, rank, ties.method="min")
+  X.sorted = apply(X, 2, sort)
+  X.mean = apply(X.sorted, 1, mean)
+  X.final = apply(X.rank, 2, function(x) X.mean[x])
+  return(X.final)
+}
